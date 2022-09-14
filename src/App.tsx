b557188ec73b46ff64b7ee6adff5a50b17e0da20
Sourabh Singh <sourabhsingh01998@gmail.com>
2022-09-14 22:40:58 +0530

    added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Animasi Start Up</p>
+    <div className="App bg-black h-full">
+      <header className='px-5 py-2'>
+        <img src={logo} className="w-12" alt="logo"  />
       </header>
     </div>
   );
