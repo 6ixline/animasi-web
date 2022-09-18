bf9a97fd3b639fdc5b670991a4f538b6fac54dc4
Sourabh Singh <sourabhsingh01998@gmail.com>
2022-09-18 22:09:29 +0530

    First push

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,68 @@
-import React from 'react';
-import logo from './logo.png';
+import React, { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import axios from 'axios';
+// @ts-ignore
+import logo from './assets/logo.png';
 import './App.css';
 
+const cleanHTML= (html: string, letterCount: number) => {
+  return html.replace(/<[^>]*>?/gm, "").substring(0, letterCount) + "...";
+};
+
 function App() {
+
+  const [trendingAnime, setTrendingAnime] = useState([]);
+  useEffect(()=>{
+    async function getTrendingAnime (){
+      try {
+        const data = await axios.get("https://consumet-api.herokuapp.com/meta/anilist/trending");
+        const results = data.data.results;
+        console.log(results)
+        if(results.length > 0){
+          await setTrendingAnime(results);          
+        }
+
+      } catch (error) {
+        console.log(error)        
+      }
+    }
+    getTrendingAnime();
+  }, [])
+
   return (
-    <div className="Apph-full">
-      <header className='px-5 py-2'>
-        <img src={logo} className="w-12" alt="logo"  />
-      </header>
-      <div className="crousel flex justify-center align-middle">
-          <p>Main Body</p>
-      </div>
+    <div className='bg-slate-900' style={{ minHeight: "100vh"}}>
+    
+      <Swiper
+       className="lg:ml-0 flex h-full relative "
+       slidesPerView={1}
+       modules={[Pagination]}
+       loop={true}
+       pagination={{ clickable: true }}
+    >
+      <img src={logo} className='w-12 rounded-lg absolute top-5 left-5 z-30 ' alt="" />
+
+      {
+       trendingAnime && trendingAnime.map((item:any)=>{
+          if (item.cover.includes("banner")) {
+          return (
+          <SwiperSlide key={item.id}>
+            <div className='rounded-xl p-3 lg:h-[26rem] h-[18rem] w-full pb-8 flex justify-center relative'>
+              <div className="relative h-full z-50 w-full flex justify-start items-start">
+                <img src={item.cover}  className="h-full rounded-lg " alt="" />
+                <p className='top-20 text-white font-noraml z-20 text-4xl left-10 absolute'>{item.title.english}</p>
+                <p className='top-40 w-2/4 text-white font-noraml z-20 text- left-10 absolute'>{cleanHTML(item.description, 700)}</p>
+                <div className="dark-gradient-banner z-10 rounded-xl absolute w-full h-full">.</div>
+              </div>
+            </div>
+          </SwiperSlide>
+        )
+          }
+        })
+      }
+    </Swiper>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        redor: "#FC4747",
+        "whole-page": "#10141E",
+        "blue-gray": "#5A698F",
+        "navy-blue": "#161D2F",
+        white: "#FFF",
+        black: "#000",
+      },
+      screens: {
+        tablet: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1822px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+  ],
 }
