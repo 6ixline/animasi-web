d4ff53dc6b15ef9e3052677f7e839b84185cfb17
Sourabh Singh <sourabhsingh01998@gmail.com>
2022-09-20 22:44:07 +0530

    Added Recent Anime

--- src/App.tsx
+++ src/App.tsx
@@ -15,51 +15,77 @@ const cleanHTML= (html: string, letterCount: number) => {
 function App() {
 
   const [trendingAnime, setTrendingAnime] = useState([]);
+  const [recentAnime, setRecentAnime] = useState([]);
   useEffect(()=>{
-    async function getTrendingAnime (){
+    async function setAnimeDataHome(){
       try {
-        const data = await axios.get("https://consumet-api.herokuapp.com/meta/anilist/trending");
-        const results = data.data.results;
-        console.log(results)
-        if(results.length > 0){
-          await setTrendingAnime(results);          
+        const trendingData = await axios.get("https://consumet-api.herokuapp.com/meta/anilist/trending");
+        const recentResult = await axios.get('https://consumet-api.herokuapp.com/meta/anilist/recent-episodes');
+        const trendingresults = trendingData.data.results;
+        const recentAnime = recentResult.data.results;
+        if(trendingresults.length > 0){
+          await setTrendingAnime(trendingresults);      
+          await setRecentAnime(recentAnime);    
         }
 
       } catch (error) {
         console.log(error)        
       }
     }
-    getTrendingAnime();
+    setAnimeDataHome();
   }, [])
 
   return (
-    <div className='bg-slate-900' style={{ minHeight: "100vh"}}>
+    <div className='bg-slate-900 p-3' style={{ minHeight: "100vh"}}>
     
-      <Swiper
+    <Swiper
        className="lg:ml-0 flex h-full relative "
        slidesPerView={1}
        modules={[Pagination]}
        loop={true}
        pagination={{ clickable: true }}
     >
-      <img src={logo} className='w-12 rounded-lg absolute top-5 left-5 z-40' alt="" />
+      <img src={logo} className='w-12 rounded-lg absolute top-5 left-5 z-50' alt="" />
 
       {
        trendingAnime && trendingAnime.map((item:any)=>{
           if (item.cover.includes("banner")) {
           return (
           <SwiperSlide key={item.id}>
-            <div className='rounded-xl p-3 lg:h-[26rem] h-[18rem] w-full pb-8 flex justify-center relative'>
-              <div className="relative h-full z-50 w-full flex justify-start items-start">
+            <div className='rounded-xl  lg:h-[24rem] h-[17rem] w-full pb-8 flex justify-center relative'>
+              <div className="relative h-full z-30 w-full flex justify-start items-start">
                 <img src={item.cover}  className="h-full rounded-lg " alt="" />
                 <p className='top-20 text-white font-noraml z-20 text-4xl left-10 absolute'>{item.title.english}</p>
-                <p className='top-40 w-2/4 text-white font-noraml z-20 text- left-10 absolute'>{cleanHTML(item.description, 700)}</p>
+                <p className='top-40 lg:w-3/4 text-white font-noraml z-20 text- left-10 absolute'>{cleanHTML(item.description, 700)}</p>
                 <div className="dark-gradient-banner z-10 rounded-xl absolute w-full h-full">.</div>
               </div>
             </div>
           </SwiperSlide>
         )
           }
+        })
+      }
+    </Swiper>
+
+    <h3 className='text-2xl font-semibold text-white'>Recent Anime</h3>
+    <Swiper
+       className="lg:ml-0 lg:mt-5 flex h-full relative"
+       slidesPerView={6}
+       modules={[Pagination]}
+       loop={true}
+       pagination={{ clickable: true }}
+       spaceBetween={10}
+    >
+      {
+       recentAnime && recentAnime.map((item:any)=>{
+          return (
+          <SwiperSlide key={item.id}>
+            <div className="w-full lg:mb-9 relative rounded-2xl " style={{background: `url(${item.image})`, height: "360px"}}>
+              <p className='absolute bottom-3 text-bold px-3 z-20 text-white'>{item.title.english && cleanHTML(item.title.english, 26)}</p>
+              <div className="backDropGradient z-10 rounded-xl absolute w-full h-full">.</div>
+            </div>
+          </SwiperSlide>
+        )
         })
       }
     </Swiper>
