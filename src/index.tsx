66da81e34ef7f6bd86465edab882f6e9721491dd
Sourabh Singh <sourabhsingh01998@gmail.com>
2022-09-25 19:05:36 +0530

    redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
